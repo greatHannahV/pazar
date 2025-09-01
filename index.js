eaf39f309ed7462576c8434dd84e7a6e9cc311ec
Hanna Veleva <i.kasl@mail.ru>
2025-09-01 21:10:39 +0300

    addedcd card page

--- index.js
+++ index.js
@@ -2,13 +2,21 @@ const fs = require('fs')
 const http = require('http')
 const url = require('url')
 
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
+const dataObj = JSON.parse(data)
 const server = http.createServer((req, res) => {
   console.log(req.url)
   const pathName = req.url
-  if (pathName === '/overview') {
+  if (pathName === '/overview' || pathName === '/') {
     res.end('overview')
   } else if (pathName === '/product') {
     res.end('product')
+  } else if (pathName === '/api') {
+    const productData = JSON.parse(data)
+    res.writeHead(200, {
+      'Content-type': 'application/json',
+    })
+    res.end(data)
   } else {
     res.writeHead(404, {
       'Content-type': 'text/html',
