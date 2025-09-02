d6cd2905ed99ed809d603c8ea286afdcb7f3edb8
Hanna Veleva <i.kasl@mail.ru>
2025-09-02 22:02:20 +0300

    created modules/replaceTemplate.js and updated index.js to use it

--- index.js
+++ index.js
@@ -1,24 +1,8 @@
 const fs = require('fs')
 const http = require('http')
 const url = require('url')
+const replaceTemplate = require('./modules/replaceTemplate')
 
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName)
-  output = output.replace(/{%PRICE%}/g, product.price)
-  output = output.replace(/{%FROM%}/g, product.from)
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients)
-  output = output.replace(/{%QUANTITY%}/g, product.quantity)
-  output = output.replace(/{%DESCRIPTION%}/g, product.description)
-  output = output.replace(/{%ID%}/g, product.id)
-  output = output.replace(/{%IMAGE%}/g, product.image)
-
-  output = output.replace(
-    /{%NOT_ORGANIC%}/g,
-    product.organic ? '' : 'not-organic',
-  )
-
-  return output
-}
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/overview.html`,
   'utf-8',
@@ -33,13 +17,14 @@ const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
 const dataObj = JSON.parse(data)
 const server = http.createServer((req, res) => {
   const { pathname, query } = url.parse(req.url, true)
-  console.log(url.parse(req.url, true))
   //overview page
   if (pathname === '/overview' || pathname === '/') {
     res.writeHead(200, {
       'Content-type': 'text/html',
     })
-    const cardsHtml = dataObj.map((el) => replaceTemplate(tempCard, el))
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join('')
     const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml)
     res.end(output)
     //product page
